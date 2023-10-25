77cb6359e2e284b7abf5fb0f2a3798387c27d229
Omkar-Jawalkar <144772416+OmkarJawalkar-Goreeva@users.noreply.github.com>
2023-10-25 13:41:30 +0530

    good

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <React.Fragment>
+            <h1 className="font-bold text-4xl text-violet-800">Hello world</h1>
+        </React.Fragment>
+    );
+}
+
+export default App;

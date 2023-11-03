ac76fec1532a75312e97337fe40f631fa76391d6
Omkar-Jawalkar <omkarjawalkar234@gmail.com>
2023-11-03 16:16:26 +0530

    Header done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { WiStars } from 'react-icons/wi';
 
 const Navbar = () => {
    return (
       <div className="flex justify-between p-6 h-full bg-blue-50">
          {/* LEFT SIDE */}
          <div className="flex space-x-1 ">
-            <span className=" font-semibold relative  text-purple-600">
-               Magic
-            </span>
-            <h1 className="text-xl transition duration-300 ease-in-out">
+            <div className="flex relative">
+               <span className=" font-semibold  text-purple-600">Magic</span>
+               <WiStars className="text-4xl absolute top-[-0.6rem] left-[9.7rem] text-purple-600 " />
+            </div>
+
+            <h1 className="text-xl text-purple-600 font-bold transition duration-300 ease-in-out">
                PaintBoard
             </h1>
          </div>
